257c1105ec28f9c727cc290602b01ce2877d16e1
Baker Moran <baker.moran@joinroot.com>
2021-01-08 10:16:43 -0500

    initialize a board from a passed in board matrix, and evaluate score on a board.
    Co-authored-by: Patrick Veith <pveith6693@gmail.com>

--- lib/chess/board.rb
+++ lib/chess/board.rb
@@ -1,6 +1,6 @@
 module Chess
   class Board
-    attr_reader :board
+    attr_reader :board_matrix
 
     WHITE_BACK_ROW_ORDER = [
       Chess::Piece::Type::ROOK,
@@ -13,20 +13,21 @@ module Chess
       Chess::Piece::Type::ROOK
     ].freeze
 
-    def initialize
-      @board = Array.new(8).map { Array.new(8) }
+    def initialize(board_matrix: nil)
+      @board_matrix = board_matrix || Array.new(8).map { Array.new(8) }
+      return if board_matrix
 
-      @board.each_with_index do |row, rank|
+      @board_matrix.each_with_index do |row, rank|
         row.each_with_index do |square, file|
           case rank
           when 0
-            @board[rank][file] = Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => WHITE_BACK_ROW_ORDER[file])
+            @board_matrix[rank][file] = Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => WHITE_BACK_ROW_ORDER[file])
           when 1
-            @board[rank][file] = Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::PAWN)
+            @board_matrix[rank][file] = Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::PAWN)
           when 6
-            @board[rank][file] = Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::PAWN)
+            @board_matrix[rank][file] = Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::PAWN)
           when 7
-            @board[rank][file] = Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => WHITE_BACK_ROW_ORDER.reverse[file])
+            @board_matrix[rank][file] = Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => WHITE_BACK_ROW_ORDER.reverse[file])
           end
         end
       end
@@ -34,7 +35,7 @@ module Chess
 
     def score
       score = 0
-      @board.each do |row|
+      @board_matrix.each do |row|
         row.each do |square|
           next if square.nil?
 
--- spec/chess/board_spec.rb
+++ spec/chess/board_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Chess::Board do
     subject { described_class.new }
 
     context "when creating a new board" do
-      let(:board) do
+      let(:board_matrix) do
         [
           [
             Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::ROOK),
@@ -37,7 +37,46 @@ RSpec.describe Chess::Board do
       end
 
       it "puts the pieces in the correct place" do
-        expect(subject.board).to eq(board)
+        expect(subject.board_matrix).to eq(board_matrix)
+      end
+    end
+
+    context "when passing in a board_matrix" do
+      subject { described_class.new(:board_matrix => board_matrix) }
+
+      let(:board_matrix) do
+        [
+          [
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::ROOK),
+            nil,
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::BISHOP),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::QUEEN),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::KING),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::BISHOP),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::KNIGHT),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::ROOK)
+          ],
+          Array.new(8) { Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::PAWN) },
+          [nil, Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::KNIGHT), nil, nil, nil, nil, nil, nil],
+          Array.new(8),
+          Array.new(8),
+          Array.new(8),
+          Array.new(8) { Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::PAWN) },
+          [
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::ROOK),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::KNIGHT),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::BISHOP),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::KING),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::QUEEN),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::BISHOP),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::KNIGHT),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::ROOK)
+          ],
+        ]
+      end
+
+      it "creates the board with that board_matrix" do
+        expect(subject.board_matrix).to eq(board_matrix)
       end
     end
   end
@@ -45,12 +84,47 @@ RSpec.describe Chess::Board do
   describe "#score" do
     subject { described_class.new }
 
+    context "when no pieces have been taken" do
+      it "returns a score of 0" do
+        expect(subject.score).to eq(0)
+      end
+    end
+
     context "when black has taken a knight" do
-      it "black has a score advantage" do
-        expect(subject.score).to eq(3)
+      subject { described_class.new(:board_matrix => board_matrix) }
+
+      let(:board_matrix) do
+        [
+          [
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::ROOK),
+            nil,
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::BISHOP),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::QUEEN),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::KING),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::BISHOP),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::KNIGHT),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::ROOK)
+          ],
+          Array.new(8) { Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::PAWN) },
+          Array.new(8),
+          Array.new(8),
+          Array.new(8),
+          Array.new(8),
+          Array.new(8) { Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::PAWN) },
+          [
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::ROOK),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::KNIGHT),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::BISHOP),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::KING),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::QUEEN),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::BISHOP),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::KNIGHT),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::ROOK)
+          ],
+        ]
       end
 
-      it "white has a score disadvantage" do
+      it "black has a score advantage" do
         expect(subject.score).to eq(-3)
       end
     end
