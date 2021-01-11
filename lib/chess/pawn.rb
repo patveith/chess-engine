2371b90a3b2412c9ff563bddae16fcebfb44e2f5
Baker Moran <baker.moran@joinroot.com>
2021-01-11 16:59:03 -0500

    add pawn attacking movements

--- lib/chess/pawn.rb
+++ lib/chess/pawn.rb
@@ -3,33 +3,53 @@ module Chess
     def available_moves(board)
       direction = @team == Chess::Piece::Team::WHITE ? 1 : -1
 
+      moves = []
+      if Chess::Board.on_board?(file_shift(1), @rank + direction) && board.piece_at_square(:file => file_shift(1), :rank => @rank + direction)
+        moves.append(
+          {
+            :file => file_shift(1),
+            :rank => @rank + direction
+          }
+        )
+      end
+      if Chess::Board.on_board?(file_shift(-1), @rank + direction) && board.piece_at_square(:file => file_shift(-1), :rank => @rank + direction)
+        moves.append(
+          {
+            :file => file_shift(-1),
+            :rank => @rank + direction
+          }
+        )
+      end
+
       if board.piece_at_square(:file => @file, :rank => @rank + direction)
-        return []
+        moves
       elsif board.piece_at_square(:file => @file, :rank => @rank + 2 * direction)
-        return [
+        return moves.append(
           {
             :file => @file,
             :rank => @rank + direction
           }
-        ]
+        )
       elsif !@moved
-        return [
+        moves.append(
           {
             :file => @file,
             :rank => @rank + 2 * direction
-          },
+          }
+        )
+        moves.append(
           {
             :file => @file,
             :rank => @rank + direction
           }
-        ]
+        )
       else
-        [
+        moves.append(
           {
             :file => @file,
             :rank => @rank + direction
           }
-        ]
+        )
       end
     end
   end
--- spec/chess/pawn_spec.rb
+++ spec/chess/pawn_spec.rb
@@ -26,7 +26,6 @@ RSpec.describe Chess::Pawn do
           }
         ]
       end
-
       it "returns two possible moves" do
         expect(subject.available_moves(board)).to match_array(available_moves)
       end
@@ -247,6 +246,88 @@ RSpec.describe Chess::Pawn do
       end
 
       it "has one available move" do
+        expect(subject.available_moves(board)).to match_array(available_moves)
+      end
+    end
+
+    context "when a pawn is threatening a piece" do
+      subject do
+        described_class.new(
+          :team => described_class::Team::WHITE,
+          :type => described_class::Type::PAWN,
+          :file => "e",
+          :rank => 4
+        )
+      end
+
+      let(:board_matrix) do
+        [
+          [
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::ROOK, :file => "a", :rank => 1),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::KNIGHT, :file => "b", :rank => 1),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::BISHOP, :file => "c", :rank => 1),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::QUEEN, :file => "d", :rank => 1),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::KING, :file => "e", :rank => 1),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::BISHOP, :file => "f", :rank => 1),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::KNIGHT, :file => "g", :rank => 1),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::ROOK, :file => "h", :rank => 1)
+          ],
+          [
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::PAWN, :file => "a", :rank => 2),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::PAWN, :file => "b", :rank => 2),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::PAWN, :file => "c", :rank => 2),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::PAWN, :file => "d", :rank => 2),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::PAWN, :file => "e", :rank => 2),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::PAWN, :file => "f", :rank => 2),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::PAWN, :file => "g", :rank => 2),
+            Chess::Piece.new(:team => Chess::Piece::Team::WHITE, :type => Chess::Piece::Type::PAWN, :file => "h", :rank => 2)
+          ],
+          Array.new(8),
+          Array.new(8),
+          [nil, nil, nil, Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::PAWN, :file => "d", :rank => 5), nil, nil, nil, nil],
+          Array.new(8),
+          Array.new(8),
+          [
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::PAWN, :file => "a", :rank => 7),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::PAWN, :file => "b", :rank => 7),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::PAWN, :file => "c", :rank => 7),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::PAWN, :file => "d", :rank => 7),
+            nil,
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::PAWN, :file => "f", :rank => 7),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::PAWN, :file => "g", :rank => 7),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::PAWN, :file => "h", :rank => 7)
+          ],
+          [
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::ROOK, :file => "a", :rank => 8),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::KNIGHT, :file => "b", :rank => 8),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::BISHOP, :file => "c", :rank => 8),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::KING, :file => "d", :rank => 8),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::QUEEN, :file => "e", :rank => 8),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::BISHOP, :file => "f", :rank => 8),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::KNIGHT, :file => "g", :rank => 8),
+            Chess::Piece.new(:team => Chess::Piece::Team::BLACK, :type => Chess::Piece::Type::ROOK, :file => "h", :rank => 8)
+          ],
+        ]
+      end
+
+      let(:board) { Chess::Board.new(:board_matrix => board_matrix) }
+
+      let(:available_moves) do
+        [
+          {
+            :file => "e",
+            :rank => 5
+          },
+          {
+            :file => "d",
+            :rank => 5
+          },
+        ]
+      end
+
+      it "has 2 available moves" do
+        subject.move_to(:file => "e", :rank => 3)
+        subject.move_to(:file => "e", :rank => 4)
         expect(subject.available_moves(board)).to match_array(available_moves)
       end
     end
